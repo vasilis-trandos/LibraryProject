beb3a6af0f51d4b188fb75b63d198cab64b4e2fd
GothPiercer <vasilhstr@hotmail.com>
2024-10-23 19:37:43 +0300

    Api Data

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,26 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Book } from './books';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule], 
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
   httpClient = inject(HttpClient);
-  books: any[] = [];
+  books: Book[] = []; 
 
   ngOnInit(): void {
     this.fetchBooks();
   }
 
   fetchBooks() {
-    this.httpClient.get('https://book-api-bx2r.onrender.com/books')
-      .subscribe((data: any) => {
+    this.httpClient.get<Book[]>('https://book-api-bx2r.onrender.com/books') 
+      .subscribe((data: Book[]) => { 
         console.log(data);
-        this.books = data;
+        this.books = data; 
       });
   }
 }
